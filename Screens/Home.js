0eac8a4e4f3be20c789b4d2a4634401cc4f017e0
unknown <edk22@calvin.edu>
2022-10-26 20:57:46 -0400

    client mqtt setup

--- Screens/Home.js
+++ Screens/Home.js
@@ -1,18 +1,49 @@
+import Paho from "paho-mqtt"
 import React from 'react';
 import { DefaultTheme, Provider as PaperProvider, Button } from 'react-native-paper';
-import {View, Text, StyleSheet} from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 import { WasherCardComponent } from '../components/WasherCard';
+import { useState, useEffect, setIte } from 'react';
+
+client = new Paho.Client(
+  "test.mosquitto.org",
+  Number(8080),
+  `washer-${parseInt(Math.random() * 100)}`
+);
 
 export function HomeScreen({ navigation }) {
+
+  const [value, setValue] = useState("off");
+
+  function onMessage(message) {
+    if (message.destinationName === "washer/value")
+      console.log("received");
+    setValue(message.payloadString);
+  }
+
+  useEffect(() => {
+    client.connect({
+      onSuccess: () => {
+        console.log("Connected!");
+        client.subscribe("washer/value");
+        client.onMessageArrived = onMessage;
+      },
+      onFailure: () => {
+        console.log("Failed to connect!");
+      }
+    });
+  }, [])
+
   return (
     <View style={style.container}>
       <Text>Washers</Text>
+      <Text>on/off value: {value}</Text>
       <Button mode="contained"
         onPress={() => navigation.openDrawer()}>
-          Press Me
+        Press Me
       </Button>
-      <WasherCardComponent/>
-      <WasherCardComponent/>
+      <WasherCardComponent />
+      <WasherCardComponent />
     </View>
   );
 }
--- components/WasherCard.js
+++ components/WasherCard.js
@@ -1,18 +1,18 @@
 import * as React from 'react';
 import { Card, Title, Paragraph } from 'react-native-paper';
-import {StyleSheet, View} from 'react-native';
+import { StyleSheet, View } from 'react-native';
 
 export function WasherCardComponent() {
-  return(
-  <View style = {styles.border}>
-  <Card style = {styles.container}>
-    <Card.Content>
-      <Title>Washer 1</Title>
-      <Paragraph>Time: 20:00</Paragraph>
-      <Card.Cover source={{ uri: 'https://picsum.photos/100/200' }} />
-    </Card.Content>
-  </Card>
-  </View>
+  return (
+    <View style={styles.border}>
+      <Card style={styles.container}>
+        <Card.Content>
+          <Title>Washer 1</Title>
+          <Paragraph>Time: 20:00</Paragraph>
+          <Card.Cover source={{ uri: 'https://picsum.photos/100/200' }} />
+        </Card.Content>
+      </Card>
+    </View>
   );
 }
 
@@ -28,6 +28,6 @@ const styles = StyleSheet.create({
   border: {
     flex: .5,
     borderWidth: 1,
-    borderColor:"#ffffff",
+    borderColor: "#ffffff",
   }
 });
