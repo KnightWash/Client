53543005f20ab3f51c7cf2000f776d7970b3d184
Brian Langejans <bel3@students.calvin.edu>
2022-12-01 23:49:28 -0500

    Can now fully switch halls!
    Can switch halls and cards change accordingly

--- Screens/Home.js
+++ Screens/Home.js
@@ -5,7 +5,7 @@ import { View, Text, StyleSheet } from 'react-native';
 import WasherCardComponent from '../components/WasherCard';
 import DryerCardComponent from '../components/DryerCard';
 import LocationDropdownComponent from '../components/LocationDropdown';
-import { useState, useEffect, setIte } from 'react';
+import { useState, useEffect, setIte, useRef } from 'react';
 import { ThemeColors } from "react-navigation";
 import { theme } from "../theme";
 import { color } from "react-native-reanimated";
@@ -22,21 +22,21 @@ client = new Paho.Client(
 export function HomeScreen({ navigation }) {
 
   const initialMachines = [];
-  const [washers, setWashers] = useState(initialMachines);
-  const [dryers, setDryers] = useState(initialMachines);
+  const [washers, setWashers] = useState([...initialMachines]);
+  const [dryers, setDryers] = useState([...initialMachines]);
 
   // TODO: hall is not initialized as soon as the app is opened
-  const [hall, setHall] = useState(beta);
+  const [hall, setHall] = useState("beta");
 
   // TODO: This should be in a database at some point.
   const bolt = new Hall("bolt", ["B/W/L", "B/D/LR"]);
   const timmer = new Hall("timmer", ["B/W/L", "B/D/LR"]);
   const beta = new Hall("beta", ["beta/washer/right", "beta/dryer/right"]);
-  const halls = [bolt, timmer, beta];
+  const gamma = new Hall("gamma", ["gamma/washer/left", "gamma/dryer/left"]);
+  const halls = [bolt, timmer, beta, gamma];
 
   function MachineComponents(props) {
     const washerList = props.machines;
-    console.log(props.machines);
     const washerMap = washerList.map((washer) =>
       <WasherCardComponent key = {washer.name} data = {washer}/>
     );
@@ -45,33 +45,86 @@ export function HomeScreen({ navigation }) {
       <View style={style.cardContainer}>
         {washerMap}
       </View>
+
     );
   }
 
   function menuCallback(hallData) {
-    client.unsubscribe("/#");
-    setHall(hallData);
+    //client.unsubscribe("/#");
+    hallRef.current = hallData.getName();
+    setDryers([]);
+    setWashers([]);
+    console.log("got to menu callback. changing hall to " + hallData.getName());
+    setHall(hallData.getName());
   }
 
   function onMessage(message) {
     const newMachine = new Machine(message.destinationName, message.payloadString);
-    //console.log(washers);
-    //console.log(washers.find(o => o.name === newMachine.name));
-    if (washers.find(o => o.getName() === newMachine.getName()) !== newMachine && message.destinationName.includes("washer")) {
-      console.log("got to set new washer");
-      setWashers([...washers, newMachine]);
-      //console.log(washers);
-    } else if (dryers.find(o => o.name === newMachine.getName()) !== newMachine && message.destinationName.includes("dryer")) {
-      setDryers([...dryers, newMachine]);
+
+    //console.log(currentSearch);
+    console.log(hallRef.current + "/washer");
+    if (message.destinationName.includes(hallRef.current + "/washer")) {
+      //console.log("Found a washer");
+      const currentWasherSearch = washersRef.current.find(o => o.name === newMachine.name);
+      let newList = washersRef.current;
+      //console.log(newList);
+      if (currentWasherSearch?.getName() === newMachine.getName()) {
+        console.log("got to update washer");
+        newList[newList.indexOf(currentWasherSearch)] = newMachine;
+        setWashers(() => [...newList]);
+      } else {
+        console.log("got to set new washer");
+        setWashers((prev) => {
+          return  [...prev, newMachine]
+        });
+      }
+    } else if (message.destinationName.includes(hallRef.current + "/dryer")) {
+      //console.log("Found a dryer");
+      const currentDryerSearch = dryersRef.current.find(o => o.name === newMachine.name);
+      let newList = dryersRef.current;
+      if (currentDryerSearch?.getName() === newMachine.getName()) {
+        console.log("got to update dryer");
+        newList[newList.indexOf(currentDryerSearch)] = newMachine;
+        setDryers(() => [...newList]);
+      } else {
+        console.log("got to set new dryer");
+        setDryers((prev) => {
+          return  [...prev, newMachine]
+        });
+      }
     }
   }
+
+  const washersRef = useRef([]);
+  useEffect(() => {
+    //console.log("got to useEffect!");
+    washersRef.current = [...washers];
+    //console.log(washersRef);
+  }, [washers]);
+
+  const dryersRef = useRef([]);
+  useEffect(() => {
+    //console.log("got to useEffect!");
+    dryersRef.current = [...dryers];
+    //console.log(washersRef);
+  }, [dryers]);
+
+  const hallRef = useRef("");
+  useEffect(() => {
+    console.log("got to useEffect line 112");
+    hallRef.current = hall;
+    console.log(hallRef.current);
+  }, [hall]);
+
+
 
   useEffect(() => {
     client.connect({
       onSuccess: () => {
         console.log("Connected!");
         // subscribe to all topics with the current hall's name
-        client.subscribe(hall.getName().concat("/#"));
+        client.subscribe("calvin/#");
+        //client.subscribe("calvin/" + hallRef.current.getName() + "/#");
         client.onMessageArrived = onMessage;
       },
       onFailure: () => {
@@ -79,17 +132,17 @@ export function HomeScreen({ navigation }) {
       }
     });
     // return () => {
-    //   client.unsubscribe("/#");
+    //    client.unsubscribe("/#");
     // };
-  }, [hall])
+  }, [])
 
   return (
     <ScrollView style={style.container} contentContainerStyle={{ flexGrow: 1 }}>
       <LocationDropdownComponent parentCallback = {menuCallback} halls={halls}></LocationDropdownComponent>
       <Text style={style.largeLabelText}>Washers</Text>
-      <MachineComponents machines={washers}></MachineComponents>
+      <MachineComponents machines={washersRef.current}></MachineComponents>
       <Text style={style.largeLabelText}>Dryers</Text>
-      <MachineComponents machines={dryers}></MachineComponents>
+      <MachineComponents machines={dryersRef.current}></MachineComponents>
     </ScrollView>
   );
 }
@@ -104,6 +157,7 @@ const style = StyleSheet.create({
     flexWrap: "wrap"
   },
   largeLabelText: {
+    flex: 1,
     color: theme.colors.largeLabelText,
     fontSize: theme.fonts.large.fontSize,
     fontFamily: theme.fonts.large.fontFamily
